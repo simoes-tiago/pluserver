64be9eed3792d34dc9f823a38fd2e1f7166d84aa
simoes-tiago <tiago.h.simoes@gmail.com>
2024-02-25 12:00:43 +0000

    added api contract

--- handlers/router.go
+++ handlers/router.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"net/http"
 	"pluserver/service"
 )
 
@@ -19,32 +18,28 @@ func InitRouter(svc service.Service) *chi.Mux {
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
+	router.Route("/api", func(api chi.Router) {
+		api.Route("/users", func(u chi.Router) {
+			u.Get("/", h.GetAllUsers)
+			u.Post("/", h.CreateUser)
+			u.Route("/{user}", func(r chi.Router) {
+				r.Get("/", h.GetUser)
+				r.Delete("/", h.DeleteUser)
+				r.Patch("/", h.UpdateUser)
+			})
+		})
 
-	router.Get("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("Hello, hello!"))
-	})
-
-	router.Route("/users", func(u chi.Router) {
-		u.Get("/", h.GetAllUsers)
-		u.Post("/", h.CreateUser)
-		u.Route("/{user}", func(r chi.Router) {
-			r.Get("/", h.GetUser)
-			r.Delete("/", h.DeleteUser)
-			r.Patch("/", h.UpdateUser)
+		api.Route("/transactions", func(t chi.Router) {
+			t.Get("/", h.GetAllTransactions)
+			t.Post("/deposit", h.CreateDeposit)
+			t.Post("/withdraw", h.CreateWithdraw)
+			t.Post("/transfer", h.CreateTransfer)
+			t.Route("/{id}", func(r chi.Router) {
+				r.Get("/", h.GetTransaction)
+				r.Delete("/", h.DeleteTransaction)
+				r.Patch("/", h.UpdateTransaction)
+			})
 		})
 	})
-
-	router.Route("/transactions", func(t chi.Router) {
-		t.Get("/", h.GetAllTransactions)
-		t.Post("/deposit", h.CreateDeposit)
-		t.Post("/withdraw", h.CreateWithdraw)
-		t.Post("/transfer", h.CreateTransfer)
-		t.Route("/{id}", func(r chi.Router) {
-			r.Get("/", h.GetTransaction)
-			r.Delete("/", h.DeleteTransaction)
-			r.Patch("/", h.UpdateTransaction)
-		})
-	})
-
 	return router
 }
